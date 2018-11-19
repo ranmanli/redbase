62e0f94676b120d29bae3f5c724a3e03f3b9c263
Ranmanli <ranmanli@outlook.com>
2018-11-19 13:05:32 -0800

    print the MBR value

--- src/interp.cc
+++ src/interp.cc
@@ -448,6 +448,7 @@ static void mk_value(NODE *node, Value &value)
          break;
       case MBR:
          value.data = (void *)node->u.VALUE.mval;
+         printf(value.data);
          break;
    }
 }
